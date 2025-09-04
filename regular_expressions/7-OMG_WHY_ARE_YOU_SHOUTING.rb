9d4b9dc8f91b1b6aa6e460f429bbf5f837e9f49c
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-09-03 23:25:17 -0500

    if statement for nil class, A_Z scan case, dos2 and chmod, intranet is still giving me permissions denied

---
+++ regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+if ARGV[0]
+  puts ARGV[0].scan(/[A-Z]/).join
+else
+  puts ""
+end
+
+#root@NecroKnightMare:/Atlas-T5/atlas-scripting/regular_expressions# ./7-OMG_WHY_ARE_YOU_SHOUTING.rb "I realLy hOpe VancouvEr posseSs Yummy Soft vAnilla Dupper Mint Ice Nutella cream" | cat -e     
+
+#ILOVESYSADMIN$
+
+#root@NecroKnightMare:/Atlas-T5/atlas-scripting/regular_expressions# ./7-OMG_WHY_ARE_YOU_SHOUTING.rb "WHAT do you SAY?" | cat -e
+
+#WHATSAY$
+
+#root@NecroKnightMare:/Atlas-T5/atlas-scripting/regular_expressions# ./7-OMG_WHY_ARE_YOU_SHOUTING.rb "cannot read you" | cat -e
+
+#$
+
+#root@NecroKnightMare:/Atlas-T5/atlas-scripting/regular_expressions# ./7-OMG_WHY_ARE_YOU_SHOUTING.rb "CAN YOU HEAR ME NOW?" | cat -e
+
+#CANYOUHEARMENOW$
