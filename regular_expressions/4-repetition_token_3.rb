390429dcad6deda95fac25aaa4d8f6e87a44ecd6
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-09-03 22:26:19 -0500

    added another repetition file, with output comments, dos2 and chmod

---
+++ regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/hbt{2,}/).join
+#root@NecroKnightMare:/mnt/c/Users/Alexa/OneDrive/Pictures/Screenshots/Atlas-T5/atlas-scripting/regular_expressions# ./4-repetition_token_3.rb hbtt
+
+#hbtt
+#root@NecroKnightMare:/mnt/c/Users/Alexa/OneDrive/Pictures/Screenshots/Atlas-T5/atlas-scripting/regular_expressions# ./4-repetition_token_3.rb hbttttt
+
+#hbttttt
+
+#
