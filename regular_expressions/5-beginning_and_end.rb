eacfe19cd2de7e152ec753eb6f2cdbe4146f3661
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-09-03 23:01:14 -0500

    added fifth file for specific outputs, dos2, chmod, output as comments

---
+++ regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/^h.n$/).join
+
+#root@NecroKnightMare:/Atlas-T5/atlas-scripting/regular_expressions# ./5-beginning_and_end.rb hn
+
+#root@NecroKnightMare:/Atlas-T5/atlas-scripting/regular_expressions# ./5-beginning_and_end.rb hbn
+
+#hbn
+
+#root@NecroKnightMare:/Atlas-T5/atlas-scripting/regular_expressions# ./5-beginning_and_end.rb hbtn
+
+#root@NecroKnightMare:/Atlas-T5/atlas-scripting/regular_expressions# ./5-beginning_and_end.rb h8n
+
+#h8n
