ae74eee8544560bab5e5a2efcfad3f9e65e71942
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-09-05 22:17:46 +0000

    fixing file 1

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt{2,}n/).join
+puts ARGV[0].scan(/hbt{^2,5}n$/).join
 #root@NecroKnightMare:/Atlas-T5/atlas-scripting/regular_expressions# ./1-repetition_token_0.rb hbtttn
 
 #hbtttn
