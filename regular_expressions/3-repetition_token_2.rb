4eb26f32bb98eb94b29634bd63c69a52d73413d7
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-09-05 22:21:04 +0000

    changed logic to +

--- regular_expressions/3-repetition_token_2.rb
+++ regular_expressions/3-repetition_token_2.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt*n/).join
+puts ARGV[0].scan(/hbt+n/).join
 #root@NecroKnightMare:/Atlas-T5/atlas-scripting/regular_expressions# ./3-repetition_token_2.rb hbtttttn
 
 # hbtttttn
